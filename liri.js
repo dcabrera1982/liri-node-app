45f5ed2e0ae43344a38857a248a8331355180bfc
David Cabrera <dcabrera1982@gmail.com>
2019-08-13 21:32:20 -0400

    add switch command

--- liri.js
+++ liri.js
@@ -8,3 +8,17 @@ var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
 var userInput = process.argv[3];
+
+switch (command) {
+    case "concert-this":
+        bandInfo();
+    break;
+    case "spotify-this-song":
+        spotifyInfo();
+    break;
+    case "movie-this":
+        movieInfo();
+    break;
+    case "do-what-it-says":
+        directions();
+}
