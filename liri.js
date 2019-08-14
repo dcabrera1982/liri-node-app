da254152788c27267dbdc66c4636f8403a131af9
David Cabrera <dcabrera1982@gmail.com>
2019-08-14 18:29:30 -0400

    update bandInfo

--- liri.js
+++ liri.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-var fs = require("fs");
-var keys = require("./keys.js");
+// var fs = require("fs");
+// var keys = require("./keys.js");
 var axios = require("axios");
-var inquirer = require("inquirer");
-var spotify = new Spotify(keys.spotify);
+// var inquirer = require("inquirer");
+// var spotify = new Spotify(keys.spotify);
 
 
 var command = process.argv[2];
@@ -12,13 +12,35 @@ var userInput = process.argv[3];
 switch (command) {
     case "concert-this":
         bandInfo();
-    break;
+        break;
     case "spotify-this-song":
         spotifyInfo();
-    break;
+        break;
     case "movie-this":
         movieInfo();
-    break;
+        break;
     case "do-what-it-says":
         directions();
+        default:
+            break;
 }
+
+
+
+function bandInfo() {
+    axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            for (let i = 0; i < response.data.length; i++) {
+                console.log(response);
+                console.log("Artist: " + (response.data[i].lineup));
+                console.log("Venue Name: " + (response.data[i].venue.name));
+                console.log("Venue Location: " + (response.data[i].venue.city) + ", " + (response.data[i].venue.region) + ", " + (response.data[i].venue.country), "\n");
+            }
+        });
+}
+
+
+
+
+
+
